b0c86b9703ed8a93181b4550a9ca85ae8cbf524a
Wilo Man :v <agustrin23@gmail.com>
2022-08-27 23:13:57 -0500

    Final

--- fabricantes.php
+++ fabricantes.php
@@ -8,6 +8,46 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
+<!-- Comienza estilo de fondo CSS -->
+    <style>
+*{
+  margin: 0;
+  padding: 0;
+  font-family: 'Poppins', sans-serif;
+}
+
+body{   
+  
+  background-image: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
+  color:white;
+  
+background-size: 400% 400%;
+animation: gradient 15s ease infinite;
+height: 08vh;    
+}
+
+.texto{
+color: white;
+text-align: center;
+margin: 300px 0;
+font-size: 28px;
+}
+
+@keyframes gradient {
+0% {
+background-position: 0% 50%;
+    }
+50% {
+background-position: 100% 50%;
+    }
+100% {
+background-position: 0% 50%;
+}
+
+  }
+</style>  
+<!-- Finaliza estilo de fondo CSS -->
+
     <title>FABRICANTE</title>
   </head>
   <body>
@@ -20,8 +60,9 @@
  <!-- inicio formulario -->
  <form action="action/isertfabricante.php" method="POST">
   <div class="mb-3">
+  <br>
        <label class="form-label">Nombre fabricante</label>
-      '<input type="text" name="inputNombre" class="form-control">
+      <input type="text" name="inputNombre" class="form-control">
     </div>
      <button type="submit" name="enviar" class="btn btn-primary">Enviar</button>
 </form>
--- productos.php
+++ productos.php
@@ -5,43 +5,110 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <!-- Comienza estilo de fondo CSS -->
+    <style>
+  *{
+    margin: 0;
+    padding: 0;
+    font-family: 'Poppins', sans-serif;
+  }
+
+  body{   
+    
+    background-image: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
+    color:white;
+    
+  background-size: 400% 400%;
+  animation: gradient 15s ease infinite;
+  height: 08vh;    
+ }
+
+.texto{
+  color: white;
+  text-align: center;
+  margin: 300px 0;
+  font-size: 28px;
+}
+
+@keyframes gradient {
+  0% {
+background-position: 0% 50%;
+      }
+  50% {
+background-position: 100% 50%;
+      }
+  100% {
+background-position: 0% 50%;
+  }
+     }
+ 
+</style>  
+<!-- Finaliza estilo de fondo CSS -->
+
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+  
 
     <title>PRODUCTO</title>
   </head>
   <body>
 
 
+  <div class="container">
+  <br>
+
+
+
+ <!-- inicio formulario -->
+ <form action="action/insertproducto.php" method="POST">
+  <div class="mb-3">
+       <label class="form-label">Nombre Producto </label>
+      <input type="text" name="inputNombre" class="form-control">
+      <br>
+      <label class="form-label">Precio </label>
+      <input type="text" name="inputPrecio" class="form-control">
+      <br>
+       <label class="form-label">Proveedor </label>
+       <input type="text" name="inputProveedor" class="form-control">
+    </div>
+
+     <button type="submit" name="enviar" class="btn btn-primary">Enviar</button>
+</form>
+<!-- fin formulario -->
+<br>
+
+<!-- inicio tabla -->
   <table class="table table-success table-striped">
   <thead>
     <tr>
-      <th scope="col">#</th>
+      <th scope="col">Codigo</th>
       <th scope="col">Nombre</th>
-      <th scope="col">precio</th>
+      <th scope="col">Precio</th>
+      <th scope="col">Proveedor</th>
     </tr>
   </thead>
   <tbody>
 
-    <?php
+     <?php
         //include es importa archio de conexion
         include('connection/connection.php');
         //variable para enlistar toda la tabla de fabricante
-        $consulta = "SELECT * FROM producto";
+        $consulta = "SELECT producto.codigo, producto.nombre, precio, fabricante.nombre as codigo_fabricante FROM producto INNER JOIN fabricante ON producto.codigo_fabricante = fabricante.codigo";
         //query de conexcion y query de listado
         $resultado = mysqli_query($connection,$consulta);
         //mientras haya algo de la tabla, seguria enlistando
         while ($fila = mysqli_fetch_array($resultado)){
+      ?>  
 
-    ?>  
-
-    pero
+    
     
 
     <tr>
       <th scope="row"><?php echo $fila["codigo"] ?></th>
       <td><?php echo $fila["nombre"] ?></td>
       <td><?php echo $fila["precio"] ?></td>
+      <td><?php echo $fila["codigo_fabricante"] ?></td>
      
     </tr>
    
@@ -49,8 +116,9 @@
 
   </tbody>
 </table>
+</div>
 
-
+<!-- final tabla fabricante -->
 
 
 
